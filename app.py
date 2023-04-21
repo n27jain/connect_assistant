fd9bddcf96246c8946f9b5a2be8c99ad75898351
Naman Jain <n27jain@edu.uwaterloo.ca>
2023-04-20 23:54:11 -0400

    done?

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, jsonify
+from cameraTest import letsDoIt
 
 app = Flask(__name__)
 
 @app.route('/prime', methods=['POST'])
 def switch():
     query = request.json.get('obj',"NA")
+    trigger(None)
     # Process query and turn appliance on/off with RPi.GPIO
+
+
+def trigger(command_string):
+    """Uses token-matching to act on a command.
+    command_string  : str, Command. For example : "fan off", "turn the light on", etc.
+    status          : str (optional), Turns port ON if status is 'on'. Else turns port OFF.
+    """
+    # Convert command to lower case and split into words
+    command_words = set(command_string.lower().split(" "))
+    # Set status to 'on' if the word 'on' is present in the command (command_words)
+    # Iterate over all appliances (ie. keys in words)
+    letsDoIt()
 
 
 if __name__ == '__main__':
--- cameraTest.py
+++ cameraTest.py
@@ -1,15 +1,18 @@
 import time
 from picamera2 import Picamera2, Preview
 
-picam = Picamera2()
 
-config = picam.create_preview_configuration()
-picam.configure(config)
+def letsDoIt():
 
-picam.start_preview(Preview.QTGL)
+    picam = Picamera2()
 
-picam.start()
-time.sleep(2)
-picam.capture_file("test-python.jpg")
+    config = picam.create_preview_configuration()
+    picam.configure(config)
 
-picam.close()
+    picam.start_preview(Preview.QTGL)
+
+    picam.start()
+    time.sleep(2)
+    picam.capture_file("test-python.jpg")
+
+    picam.close()
